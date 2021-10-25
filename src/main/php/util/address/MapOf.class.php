3c9ce042ffca84c742ae22b65c0694ca1206d2c9
Timm Friebe <thekid@thekid.de>
2021-10-25 22:06:45 +0200

    Add util.address.MapOf to create maps

---
+++ src/main/php/util/address/MapOf.class.php
@@ -0,0 +1,58 @@
+<?php namespace util\address;
+
+use lang\Reflection;
+
+/**
+ * Creates a map based on given addresses.
+ *
+ * @test  util.address.unittest.MapOfTest
+ */
+class MapOf implements Definition {
+  private $constructor, $addresses;
+
+  /**
+   * Creates a new map definition
+   *
+   * @param  [:function(util.address.Iteration): void] $addresses
+   */
+  public function __construct($addresses) {
+    $this->addresses= $addresses;
+  }
+
+  /**
+   * Address a given path. If nothing is defined, discard value silently.
+   *
+   * @param  [:var] $map
+   * @param  string $path
+   * @param  util.address.Iteration $iteration
+   * @return void
+   */
+  protected function next(&$map, $path, $iteration) {
+    if (isset($this->addresses[$path])) {
+      foreach ($this->addresses[$path]($iteration) as $name => $value) {
+        $map[$name]= $value;
+      }
+    } else {
+      $iteration->next();
+    }
+  }
+
+  /**
+   * Creates a value from a given iteration
+   *
+   * @param  util.address.Iteration $iteration
+   * @return object
+   */
+  public function create($iteration) {
+    $map= [];
+    $base= $iteration->path().'/';
+    $length= strlen($base);
+
+    $this->next($map, '.', $iteration);
+    while (null !== ($path= $iteration->path()) && 0 === strncmp($path, $base, $length)) {
+      $this->next($map, substr($iteration->path(), $length), $iteration);
+    }
+
+    return $map;
+  }
+}
---
+++ src/test/php/util/address/unittest/MapOfTest.class.php
@@ -0,0 +1,41 @@
+<?php namespace util\address\unittest;
+
+use unittest\{Assert, Test};
+use util\address\{MapOf, XmlString};
+
+class MapOfTest {
+
+  #[Test]
+  public function compact_form() {
+    $address= new XmlString('<book>Name</book>');
+    Assert::equals(
+      ['name' => 'Name'],
+      $address->next(new MapOf([
+        '.' => function($it) { return ['name' => $it->next()]; }
+      ]))
+    );
+  }
+
+  #[Test]
+  public function child_node() {
+    $address= new XmlString('<book><name>Name</name></book>');
+    Assert::equals(
+      ['name' => 'Name'],
+      $address->next(new MapOf([
+        'name'    => function($it) { return ['name' => $it->next()]; }
+      ]))
+    );
+  }
+
+  #[Test]
+  public function child_node_and_attributes() {
+    $address= new XmlString('<book author="Test"><name>Name</name></book>');
+    Assert::equals(
+      ['name' => 'Name', 'author' => 'Test'],
+      $address->next(new MapOf([
+        '@author' => function($it) { return ['author' => $it->next()]; },
+        'name'    => function($it) { return ['name' => $it->next()]; }
+      ]))
+    );
+  }
+}
