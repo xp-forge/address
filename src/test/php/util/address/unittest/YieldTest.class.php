46dfb00580b877d7bfbf813a3db644c144971c40
Timm Friebe <thekid@thekid.de>
2021-11-14 13:55:38 +0100

    Extend test suite

--- src/test/php/util/address/unittest/YieldTest.class.php
+++ src/test/php/util/address/unittest/YieldTest.class.php
@@ -5,41 +5,63 @@ use util\Date;
 use util\address\{ValueOf, XmlString};
 
 class YieldTest {
+  const BOOK = '<book id="1"><name>Name</name><author/><date>1977-12-14</date></book>';
 
   #[Test]
   public function yield_without_argument() {
-    $address= new XmlString('<book><name>Name</name></book>');
+    $address= new XmlString(self::BOOK);
     Assert::equals(
       ['name' => 'Name'],
       $address->next(new ValueOf([], [
-        'name' => function(&$self, $it) { $self['name']= yield; }
+        'name'   => function(&$self) { $self['name']= yield; },
       ]))
     );
   }
 
   #[Test]
-  public function yield_with_definition() {
-    $address= new XmlString('<book><name>Name</name><author>Test</author></books>');
+  public function yield_with_cast() {
+    $address= new XmlString(self::BOOK);
     Assert::equals(
-      ['name' => 'Name', 'author' => 'Test'],
+      ['id' => 1],
       $address->next(new ValueOf([], [
-        '.' => function(&$self, $it) {
-          $self= yield new ValueOf(null, ['*' => function(&$self, $it, $path) {
-            $self[$path]= yield;
-          }]);
-        }
+        '@id'    => function(&$self) { $self['id']= (int)yield; },
+      ]))
+    );
+  }
+
+  #[Test]
+  public function yield_with_null_coalesce() {
+    $address= new XmlString(self::BOOK);
+    Assert::equals(
+      ['author' => '(unknown)'],
+      $address->next(new ValueOf([], [
+        'author' => function(&$self) { $self['author']= yield ?? '(unknown)'; },
       ]))
     );
   }
 
   #[Test]
   public function yield_inside_arguments() {
-    $address= new XmlString('<book><name>Name</name><date>1977-12-14</date></book>');
+    $address= new XmlString(self::BOOK);
     Assert::equals(
-      ['name' => 'Name', 'date' => new Date('1977-12-14')],
+      ['date' => new Date('1977-12-14')],
       $address->next(new ValueOf([], [
-        'name' => function(&$self, $it) { $self['name']= yield; },
-        'date' => function(&$self, $it) { $self['date']= new Date(yield); },
+        'date' => function(&$self) { $self['date']= new Date(yield); },
+      ]))
+    );
+  }
+
+  #[Test]
+  public function yield_with_definition() {
+    $address= new XmlString(self::BOOK);
+    Assert::equals(
+      ['name' => 'Name', 'author' => null, 'date' => '1977-12-14'],
+      $address->next(new ValueOf([], [
+        '.' => function(&$self) {
+          $self= yield new ValueOf(null, ['*' => function(&$self, $it, $path) {
+            $self[$path]= yield;
+          }]);
+        },
       ]))
     );
   }
