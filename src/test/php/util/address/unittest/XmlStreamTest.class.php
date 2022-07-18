4b8adc2ccc5b1cdd9e7a7faaf616afb2e8843343
Timm Friebe <t.friebe@enbw.com>
2022-07-18 21:19:36 +0200

    Add tests for XmlStream iteration

---
+++ src/test/php/util/address/unittest/XmlStreamTest.class.php
@@ -0,0 +1,48 @@
+<?php namespace util\address\unittest;
+
+use io\streams\MemoryInputStream;
+use unittest\{Assert, Test};
+use util\address\XmlStream;
+
+class XmlStreamTest {
+
+  /**
+   * Creates a new fixture
+   *
+   * @param  string $bytes
+   * @return util.address.XmlStream
+   */
+  private function fixture($bytes= '') {
+    return new XmlStream(new MemoryInputStream($bytes));
+  }
+
+  #[Test]
+  public function can_create() {
+    $this->fixture();
+  }
+
+  #[Test]
+  public function hash_of() {
+    Assert::notEquals('', $this->fixture()->hashCode());
+  }
+
+  #[Test]
+  public function string_representation() {
+    Assert::equals(
+      'util.address.XmlStream<io.streams.MemoryInputStream(@0 of 4 bytes)>',
+      $this->fixture('Test')->toString()
+    );
+  }
+
+  #[Test]
+  public function equals_itself() {
+    $fixture= $this->fixture();
+    Assert::equals($fixture, $fixture);
+  }
+
+  #[Test]
+  public function iteration() {
+    $fixture= $this->fixture('<root><a>Test</a></root>');
+    Assert::equals(['/' => null, '//a' => 'Test'], iterator_to_array($fixture));
+  }
+}
