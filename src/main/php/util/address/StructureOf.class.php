df77d0b6ada9a4a2983234e436d3d5fda0c2f387
Timm Friebe <thekid@thekid.de>
2023-02-26 19:22:24 +0100

    Add StructureOf definition

---
+++ src/main/php/util/address/StructureOf.class.php
@@ -0,0 +1,45 @@
+<?php namespace util\address;
+
+/**
+ * Creates a generic data structure from the iteration
+ *
+ * @test  util.address.unittest.StructureOfTest
+ */
+class StructureOf implements Definition {
+
+  /**
+   * Creates a value from a given iteration
+   *
+   * @param  util.address.Iteration $iteration
+   * @return var
+   */
+  public function create($iteration) {
+    $base= $iteration->path().'/';
+    $offset= strlen($base);
+    $value= $iteration->next();
+
+    while (null !== ($path= $iteration->path()) && 0 === strncmp($path, $base, $offset)) {
+      if (0 === substr_compare($path, '/[]', -3, 3)) {
+        $segments= substr($path, $offset, -3);
+        $array= true;
+      } else {
+        $segments= substr($path, $offset);
+        $array= false;
+      }
+
+      $ptr= &$value;
+      if (strlen($segments) > 0) {
+        foreach (explode('/', $segments) as $segment) {
+          $ptr= &$ptr[$segment];
+        }
+      }
+
+      if ($array) {
+        $ptr[]= $this->create($iteration);
+      } else {
+        $ptr= $iteration->next();
+      }
+    }
+    return $value;
+  }
+}
